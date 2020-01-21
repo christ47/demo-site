c82773dacac6b85790f0cc220be345111f12bb5a
christ47 <43855736+christ47@users.noreply.github.com>
2020-01-21 23:18:56 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+// const app = express();
+// on click get quote email button
+
+// New graph that highlights when hovered
+//
